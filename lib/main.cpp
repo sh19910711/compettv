ad391274fa1a0282a5b437466cd54ad611fa3ede
Hiroyuki Sano <sh19910711@gmail.com>
2015-10-11 14:07:13 +0900

    lib: edit main.cpp

--- lib/main.cpp
+++ lib/main.cpp
@@ -1,12 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define rep(i, n) for (int i = 0; i < n; ++i)
+typedef long long int_t;
+
 bool input() {
   return false;
 }
 
+void init() {
+}
+
+void solve() {
+}
+
+void output() {
+}
+
 int main() {
   while (input()) {
+    init();
+    solve();
+    output();
   }
   return 0;
 }
