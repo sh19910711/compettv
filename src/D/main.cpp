d91e4496888cb5a595435d765ee755ba4db546ba
Hiroyuki Sano <sh19910711@gmail.com>
2015-10-26 11:31:30 +0900

    wip

--- src/D/main.cpp
+++ src/D/main.cpp
@@ -5,11 +5,10 @@ using namespace std;
 typedef long long int_t;
 
 namespace {
-  double x1, y1, x2, y2;
-  double v_max;
-  int t;
-  double vx, vy;
-  double wx, wy;
+  int x1, y1, x2, y2;
+  int v_max, t;
+  int vx, vy;
+  int wx, wy;
 
   bool input() {
     if (cin >> x1 >> y1 >> x2 >> y2) {
@@ -33,10 +32,6 @@ namespace {
     return sqrt(x(v) * x(v) + y(v) * y(v));
   }
 
-  vector_t norm(const vector_t& v) {
-    return vector_t(x(v) / size(v), y(v) / size(v));
-  }
-
   void print(const vector_t& v) {
     cout << "(" << x(v) << ", " << y(v) << ")" << endl;
   }
@@ -45,26 +40,29 @@ namespace {
     return vector_t(x(v) * k, y(v) * k);
   }
 
-  vector_t diff(const vector_t& a, const vector_t& b) {
+  vector_t sub(const vector_t& a, const vector_t& b) {
     return vector_t(x(a) - x(b), y(a) - y(b));
   }
 
-  vector_t add(const vector_t& a, const vector_t& b) {
-    return vector_t(x(a) + x(b), y(a) + y(b));
-  }
-
   double solve() {
-    vector_t v = diff(x1, y1, x2, y2);
-    double need = size(v);
-    v = mul(norm(v), v_max);
-    vector_t v1 = add(v, vector_t(vx, vy));
-    double len = size(v1) * t;
-    if (len >= need) {
-      return need / size(v1);
-    } else {
-      vector_t v2 = add(v, vector_t(wx, wy));
-      return t + (need - len) / size(v2);
+    double l = 0.0;
+    double r = 1e12;
+    rep (k, 1e6) {
+      double mid = (l + r) / 2.0;
+      vector_t d = diff(x1, y1, x2, y2);
+      if (mid > t) {
+        d = sub(d, mul(vector_t(vx, vy), t));
+        d = sub(d, mul(vector_t(wx, wy), mid - t));
+      } else {
+        d = sub(d, mul(vector_t(vx, vy), mid));
+      }
+      if (size(d) > v_max * mid) {
+        l = mid;
+      } else {
+        r = mid;
+      }
     }
+    return (l + r) / 2.0;
   }
 }
 
@@ -72,7 +70,7 @@ int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
   while (input()) {
-    cout << solve() << endl;
+    printf("%.12f\n", solve());
   }
   return 0;
 }
--- src/E/main.cpp
+++ src/E/main.cpp
@@ -16,40 +16,51 @@ bool input() {
   return false;
 }
 
-typedef tuple<int, int, int, int> node_t;
-typedef priority_queue<node_t, vector<node_t>, greater<node_t>> queue_t;
+typedef tuple<int, int, int> node_t;
+typedef set<node_t> queue_t;
 
-inline int steps(const node_t& x) { return get<0>(x); }
-inline int row(const node_t& x) { return get<1>(x); }
-inline int col(const node_t& x) { return get<2>(x); }
-inline int num(const node_t& x) { return get<3>(x); }
+int mdist[3][H_MAX][H_MAX];
 
-queue_t q;
-int mc[H_MAX][H_MAX];
-
-int solve() {
-  const int inf = 1e9;
-  rep (i, H_MAX) fill(mc[i], mc[i]+H_MAX, inf);
-  q = queue_t();
-  const int dr[4] = {0, 0, 1, -1};
-  const int dc[4] = {1, -1, 0, 0};
-  int gn = 0;
-  rep (i, h) rep (j, w) if (isdigit(t[i][j])) ++gn;
-  while (!q.empty()) {
-    auto node = q.top(); q.pop();
-    auto s = steps(node);
-    auto r = row(node);
-    auto c = col(node);
-    auto n = num(node);
-    if (n == gn) return s;
-    for (int k = 0; k < 4; ++k) {
-      auto ns = s;
-      auto nr = r + dr[k];
-      auto nc = c + dc[k];
-      auto nn = n;
+void get_dist(char start) {
+  int id = start - '1';
+  queue_t q;
+  rep (i, h) rep (j, w) {
+    if (t[i][j] == start) {
+      mdist[id][i][j] = 0;
+      q.emplace(0, i, j);
     }
   }
-  return -1;
+  while (!q.empty()) {
+    int s = get<0>(*q.begin());
+    int r = get<1>(*q.begin());
+    int c = get<2>(*q.begin());
+    q.erase(q.begin());
+    const int dr[4] = {0, 0, 1, -1};
+    const int dc[4] = {1, -1, 0, 0};
+    rep (k, 4) {
+      int nr = r + dr[k];
+      int nc = c + dc[k];
+      if (nr < 0 || nr >= h || nc < 0 || nc >= w) continue;
+      if (t[nr][nc] == '#') continue;
+      int ns = s + (t[nr][nc] == '.');
+      if (mdist[id][nr][nc] <= ns) continue;
+      mdist[id][nr][nc] = ns;
+      q.emplace(ns, nr, nc);
+    }
+  }
+}
+
+int solve() {
+  const int inf = 1e7;
+  rep (i, 3) rep (j, H_MAX) fill(mdist[i][j], mdist[i][j]+H_MAX, inf);
+  for (char c = '1'; c <= '3'; ++c) {
+    get_dist(c);
+  }
+  int res = 0;
+  rep (k, 3) rep (i, h) rep (j, w) if (t[i][j] == '.') {
+    res += mdist[k][i][j];
+  }
+  return res;
 }
 
 int main() {
